0194613dffc02d911a6bc29ea062ff7ab48d0d54
Sagnik Pal <sagnikpal2004@outlook.com>
2024-11-10 01:36:47 -0500

    launch into login first

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
+import { useRouter } from "expo-router";
 import Home from "./Home";
+import { useEffect } from "react";
+import { isLoggedIn } from "@/services/auth";
 
 export default function HomeScreen() {
-
   const router = useRouter();
   useEffect(() => {(async () => { 
     if (!(await isLoggedIn()))
