d364db86ed0abe028b3e1cbd96db83eec6558906
VihSej <s.personal3104@gmail.com>
2024-11-09 22:51:09 -0500

    made a Home.tsx and simplified index.tsx

---
+++ components/Home.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import AddThingButton from "@/components/AddThingButton";
+import AddThingView from "@/components/AddThingView";
+import Header from "@/components/Header";
+import Menu from "@/components/Menu";
+import SpaceList from "@/components/SpaceList";
+import { Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+export default function Home() {
+  const [isMenuVisible, setMenuVisible] = useState(false);
+  const [isSpacesVisible, setSpacesVisible] = useState(false);
+  const [isAddThingVisible, setAddThingVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Header
+        onPressMenu={setMenuVisible}
+        onPressSpaceList={setSpacesVisible}
+      />
+      <Menu
+        visible={isMenuVisible}
+        onClose={() => {
+          setMenuVisible(false);
+          setSpacesVisible(false);
+        }}
+      />
+      <SpaceList
+        visible={isSpacesVisible}
+        onClose={() => {
+          setMenuVisible(false);
+          setSpacesVisible(false);
+        }}
+      />
+      <View style={styles.internalContainer}></View>
+      <AddThingView
+        visible={isAddThingVisible}
+        onClose={() => setAddThingVisible(false)}
+        onConfirm={() => {
+          setAddThingVisible(false);
+          console.log("New thing added!");
+        }}
+      />
+      <AddThingButton onPress={() => setAddThingVisible(true)} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  internalContainer: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+});
